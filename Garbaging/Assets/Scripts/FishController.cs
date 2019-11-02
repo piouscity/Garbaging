3326bc25bf03d61b4cafda0a8d2b8521aeeeb82a
Văn Hoằng Trương <tvhoang2112@gmail.com>
2019-11-02 14:46:20 +0700

    update

---
+++ Garbaging/Assets/Scripts/FishController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject fishType1;
+	public GameObject fishType2;
+	public GameObject fishType3;
+    public int totalFishSize = 5;
+    private Vector2 position = new Vector2(0f, 0f);
+	public List<GameObject> fishList;
+	void Start()
+    {
+        for (int i = 0; i < totalFishSize; i++)
+		{
+            fishList.Add((GameObject)Instantiate(fishType1, new Vector2(Random.Range(-GameManager.instance.maxX, GameManager.instance.maxX), Random.Range(GameManager.instance.minY, GameManager.instance.maxY)), Quaternion.identity));
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Garbaging/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public float maxX = 7.0f;
+    public float maxY = 2f;
+    public float minY = -4.5f;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
