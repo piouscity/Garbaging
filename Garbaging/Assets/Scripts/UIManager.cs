639b78f5b809dd205125286d7887f17ffdb81cae
Trung-Hieu Ho <piouscity@outlook.com>
2019-11-03 21:47:01 +0700

    make menu button functional

---
+++ Garbaging/Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadSceneWithID(int scene_id)
+    {
+        SceneManager.LoadScene(scene_id);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
