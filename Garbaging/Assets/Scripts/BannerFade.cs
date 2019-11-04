94f200924bea8a1c18be1da3ccf56047f3203435
Trung-Hieu Ho <piouscity@outlook.com>
2019-11-04 22:30:39 +0700

    add loading banner

---
+++ Garbaging/Assets/Scripts/BannerFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BannerFade : MonoBehaviour
+{
+    public Image banner;
+    public Text banner_text;
+    public float effect_time;
+    void Start()
+    {
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade()
+    {
+        for(int i=0; i<2; ++i)
+        {
+            banner_text.CrossFadeAlpha(0, 1, false);
+            yield return new WaitForSeconds(effect_time);
+            banner_text.CrossFadeAlpha(1, 1, false);
+            yield return new WaitForSeconds(effect_time);
+        }
+        banner.CrossFadeAlpha(0, 1, false);
+        banner_text.enabled = false;
+    }
+}
