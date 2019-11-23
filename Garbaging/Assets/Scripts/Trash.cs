16e5c54ca512111ad9a390f45147b3afb7e3ccc5
Trung-Hieu Ho <piouscity@outlook.com>
2019-11-23 11:14:45 +0700

    add trash script again

---
+++ Garbaging/Assets/Scripts/Trash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public TrashController manager;
+    Vector2 posTemp;
+    bool isChoose = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posTemp = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChoose)
+        {
+            posTemp.y += 3 * GameManager.instance.hookController.GetComponent<Hook>().speedHook;
+            GetComponent<Transform>().position = posTemp;
+            if (posTemp.y >= -GameManager.instance.minY)
+            {
+                Destroy(gameObject);
+                manager.RemoveTrash(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hook"))
+        {
+            isChoose = true;
+        }
+    }
+}
